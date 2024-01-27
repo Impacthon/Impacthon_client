21f8a20009ddbec05d52f7354169c0c956714f57
kimgyuha <kimgyuha25@gmail.com>
2024-01-28 07:28:03 +0900

    fix: header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GlobalStyles from './public/Globalstyle';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./public/Globalstyle";
 
-import Signin from './page/Signin';
-import Signup from './page/Signup';
-import SelectPosition from './page/survey/SelectPositon';
-import Privacyadmin from './page/survey/PrivacyGuide';
-import PrivacyTraveler from './page/survey/PrivacyTraveler';
-import ProfileDataGuid from './page/survey/ProfileDataGuid';
-import ProfileDataTraveler from './page/survey/ProfileDataTraveler';
-import Point from './page/point';
+import Signin from "./page/Signin";
+import Signup from "./page/Signup";
+import SelectPosition from "./page/survey/SelectPositon";
+import Privacyadmin from "./page/survey/PrivacyGuide";
+import PrivacyTraveler from "./page/survey/PrivacyTraveler";
+import ProfileDataGuid from "./page/survey/ProfileDataGuid";
+import ProfileDataTraveler from "./page/survey/ProfileDataTraveler";
+import Point from "./page/point";
+import Header from "./page/header/index";
 
 function App() {
   return (
@@ -23,10 +24,14 @@ function App() {
           <Route path="/Privacyadmin" element={<Privacyadmin />} />
           <Route path="/PrivacyTraveler" element={<PrivacyTraveler />} />
           <Route path="/ProfileDataGuid" element={<ProfileDataGuid />} />
-          <Route path="/ProfileDataTraveler" element={<ProfileDataTraveler />} />
+          <Route path="/" element={<Header />} />
+          <Route
+            path="/ProfileDataTraveler"
+            element={<ProfileDataTraveler />}
+          />
         </Routes>
       </Router>
-    </div> 
+    </div>
   );
 }
 
